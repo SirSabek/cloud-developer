cbd4d2dcf707c4c7c83a61c9cecd981282b79ffb
SirSabek <mmsabek99@gmail.com>
2022-09-11 18:41:23 +0200

    updating frontend

--- course-04/project/c4-final-project-starter-code/client/src/config.ts
+++ course-04/project/c4-final-project-starter-code/client/src/config.ts
@@ -1,11 +1,11 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
+const apiId = 'ink889ag6d'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map. For example:
   // domain: 'dev-nd9990-p4.us.auth0.com',
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-t3wvv7vm.us.auth0.com',            // Auth0 domain
+  clientId: 'MkQ9OKjv8cdrT8e32lT0OOS74fdAMl8g',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
